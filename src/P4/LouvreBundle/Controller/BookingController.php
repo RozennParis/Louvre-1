195dd261f59c0a50345dde2dcadb9af1d18bdf65
stephanie houssin <stephanie.houssin@gmx.fr>
2018-03-12 14:33:40 +0100

    route et controller accueil

---
+++ src/P4/LouvreBundle/Controller/BookingController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace P4\LouvreBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class BookingController extends Controller
+{
+    public function homeAction()
+    {
+    return $this->render('P4LouvreBundle:Booking:home.html.twig');
+    }
+}
