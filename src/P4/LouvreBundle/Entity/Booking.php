1a1a5b314ecba2df36c2881e8a8d26b6faac1e98
stephanie houssin <stephanie.houssin@gmx.fr>
2018-03-12 16:07:50 +0100

    création entités Ticket et Booking + génération tables et éxécution requêtes

---
+++ src/P4/LouvreBundle/Entity/Booking.php
@@ -0,0 +1,283 @@
+<?php
+
+namespace P4\LouvreBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Booking
+ *
+ * @ORM\Table(name="booking")
+ * @ORM\Entity(repositoryClass="P4\LouvreBundle\Repository\BookingRepository")
+ */
+class Booking
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="visitDate", type="datetime")
+     */
+    private $visitDate;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="reservationDate", type="datetimetz")
+     */
+    private $reservationDate;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="reservationCode", type="string", length=255)
+     */
+    private $reservationCode;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="ticketsNumber", type="integer")
+     */
+    private $ticketsNumber;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="totalAmount", type="integer")
+     */
+    private $totalAmount;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="ticketsType", type="boolean")
+     */
+    private $ticketsType;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="tickets", type="string", length=255)
+     */
+    private $tickets;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set visitDate
+     *
+     * @param \DateTime $visitDate
+     *
+     * @return Booking
+     */
+    public function setVisitDate($visitDate)
+    {
+        $this->visitDate = $visitDate;
+
+        return $this;
+    }
+
+    /**
+     * Get visitDate
+     *
+     * @return \DateTime
+     */
+    public function getVisitDate()
+    {
+        return $this->visitDate;
+    }
+
+    /**
+     * Set reservationDate
+     *
+     * @param \DateTime $reservationDate
+     *
+     * @return Booking
+     */
+    public function setReservationDate($reservationDate)
+    {
+        $this->reservationDate = $reservationDate;
+
+        return $this;
+    }
+
+    /**
+     * Get reservationDate
+     *
+     * @return \DateTime
+     */
+    public function getReservationDate()
+    {
+        return $this->reservationDate;
+    }
+
+    /**
+     * Set reservationCode
+     *
+     * @param string $reservationCode
+     *
+     * @return Booking
+     */
+    public function setReservationCode($reservationCode)
+    {
+        $this->reservationCode = $reservationCode;
+
+        return $this;
+    }
+
+    /**
+     * Get reservationCode
+     *
+     * @return string
+     */
+    public function getReservationCode()
+    {
+        return $this->reservationCode;
+    }
+
+    /**
+     * Set ticketsNumber
+     *
+     * @param integer $ticketsNumber
+     *
+     * @return Booking
+     */
+    public function setTicketsNumber($ticketsNumber)
+    {
+        $this->ticketsNumber = $ticketsNumber;
+
+        return $this;
+    }
+
+    /**
+     * Get ticketsNumber
+     *
+     * @return int
+     */
+    public function getTicketsNumber()
+    {
+        return $this->ticketsNumber;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     *
+     * @return Booking
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set totalAmount
+     *
+     * @param integer $totalAmount
+     *
+     * @return Booking
+     */
+    public function setTotalAmount($totalAmount)
+    {
+        $this->totalAmount = $totalAmount;
+
+        return $this;
+    }
+
+    /**
+     * Get totalAmount
+     *
+     * @return int
+     */
+    public function getTotalAmount()
+    {
+        return $this->totalAmount;
+    }
+
+    /**
+     * Set ticketsType
+     *
+     * @param boolean $ticketsType
+     *
+     * @return Booking
+     */
+    public function setTicketsType($ticketsType)
+    {
+        $this->ticketsType = $ticketsType;
+
+        return $this;
+    }
+
+    /**
+     * Get ticketsType
+     *
+     * @return bool
+     */
+    public function getTicketsType()
+    {
+        return $this->ticketsType;
+    }
+
+    /**
+     * Set tickets
+     *
+     * @param string $tickets
+     *
+     * @return Booking
+     */
+    public function setTickets($tickets)
+    {
+        $this->tickets = $tickets;
+
+        return $this;
+    }
+
+    /**
+     * Get tickets
+     *
+     * @return string
+     */
+    public function getTickets()
+    {
+        return $this->tickets;
+    }
+}
+
---
+++ src/P4/LouvreBundle/Entity/Ticket.php
@@ -0,0 +1,252 @@
+<?php
+
+namespace P4\LouvreBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Ticket
+ *
+ * @ORM\Table(name="ticket")
+ * @ORM\Entity(repositoryClass="P4\LouvreBundle\Repository\TicketRepository")
+ */
+class Ticket
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="firstName", type="string", length=255)
+     */
+    private $firstName;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="lastName", type="string", length=255)
+     */
+    private $lastName;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="country", type="string", length=255)
+     */
+    private $country;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="birthDate", type="datetime")
+     */
+    private $birthDate;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="reducedPrice", type="boolean")
+     */
+    private $reducedPrice;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="price", type="integer")
+     */
+    private $price;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="booking", type="string", length=255)
+     */
+    private $booking;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set firstName
+     *
+     * @param string $firstName
+     *
+     * @return Ticket
+     */
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    /**
+     * Get firstName
+     *
+     * @return string
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * Set lastName
+     *
+     * @param string $lastName
+     *
+     * @return Ticket
+     */
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    /**
+     * Get lastName
+     *
+     * @return string
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * Set country
+     *
+     * @param string $country
+     *
+     * @return Ticket
+     */
+    public function setCountry($country)
+    {
+        $this->country = $country;
+
+        return $this;
+    }
+
+    /**
+     * Get country
+     *
+     * @return string
+     */
+    public function getCountry()
+    {
+        return $this->country;
+    }
+
+    /**
+     * Set birthDate
+     *
+     * @param \DateTime $birthDate
+     *
+     * @return Ticket
+     */
+    public function setBirthDate($birthDate)
+    {
+        $this->birthDate = $birthDate;
+
+        return $this;
+    }
+
+    /**
+     * Get birthDate
+     *
+     * @return \DateTime
+     */
+    public function getBirthDate()
+    {
+        return $this->birthDate;
+    }
+
+    /**
+     * Set reducedPrice
+     *
+     * @param boolean $reducedPrice
+     *
+     * @return Ticket
+     */
+    public function setReducedPrice($reducedPrice)
+    {
+        $this->reducedPrice = $reducedPrice;
+
+        return $this;
+    }
+
+    /**
+     * Get reducedPrice
+     *
+     * @return bool
+     */
+    public function getReducedPrice()
+    {
+        return $this->reducedPrice;
+    }
+
+    /**
+     * Set price
+     *
+     * @param integer $price
+     *
+     * @return Ticket
+     */
+    public function setPrice($price)
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    /**
+     * Get price
+     *
+     * @return int
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    /**
+     * Set booking
+     *
+     * @param string $booking
+     *
+     * @return Ticket
+     */
+    public function setBooking($booking)
+    {
+        $this->booking = $booking;
+
+        return $this;
+    }
+
+    /**
+     * Get booking
+     *
+     * @return string
+     */
+    public function getBooking()
+    {
+        return $this->booking;
+    }
+}
+
