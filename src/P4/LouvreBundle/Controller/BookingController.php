3b76ed29cfd3e86045f5ff5e5717d2b9a53520b6
stephanie houssin <stephanie.houssin@gmx.fr>
2018-03-13 19:29:07 +0100

    création formulaire 1 booking - bookingType

--- src/P4/LouvreBundle/Controller/BookingController.php
+++ src/P4/LouvreBundle/Controller/BookingController.php
@@ -2,7 +2,10 @@
 
 namespace P4\LouvreBundle\Controller;
 
+use P4\LouvreBundle\Entity\Booking;
+use P4\LouvreBundle\Form\BookingType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class BookingController extends Controller
 {
@@ -10,9 +13,27 @@ class BookingController extends Controller
     {
         return $this->render('P4LouvreBundle:Booking:homepage.html.twig');
     }
-    public function ticketingAction()
+
+    /**
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function ticketingAction(Request $request)
     {
-        return $this->render('P4LouvreBundle:Booking:ticketing.html.twig');
+        $booking = new Booking();
+        $form = $this->createForm(BookingType::class,$booking);
+
+        if($request->isMethod('POST') && $form->handleRequest($request)->isValid())
+            {
+                $em= $this->getDoctrine()->getManager();
+                $em->persist($booking);
+                $em->flush();
+
+                $request->getSession()->getFlashBag()->add('commande','ticketing ok');
+                return $this->redirectToRoute('p4_louvre_visitorInfo',array('id' => $booking->getId()));
+            }
+                return $this->render('P4LouvreBundle:Booking:ticketing.html.twig',array('form' => $form->createView(),
+        ));
     }
     public function visitorInfoAction()
     {
