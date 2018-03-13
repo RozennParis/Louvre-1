4c0daff827acd04eb8ce15471b7b90d91cea2aa2
stephanie houssin <stephanie.houssin@gmx.fr>
2018-03-13 18:00:57 +0100

    Routes, vues et méthode controller->vues

--- src/P4/LouvreBundle/Controller/BookingController.php
+++ src/P4/LouvreBundle/Controller/BookingController.php
@@ -6,8 +6,24 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class BookingController extends Controller
 {
-    public function homeAction()
+    public function homepageAction()
     {
-    return $this->render('P4LouvreBundle:Home:home.html.twig');
+        return $this->render('P4LouvreBundle:Booking:homepage.html.twig');
+    }
+    public function ticketingAction()
+    {
+        return $this->render('P4LouvreBundle:Booking:ticketing.html.twig');
+    }
+    public function visitorInfoAction()
+    {
+        return $this->render('P4LouvreBundle:Booking:visitorInfo.html.twig');
+    }
+    public function orderSummaryAction()
+    {
+        return $this->render('P4LouvreBundle:Booking:orderSummary.html.twig');
+    }
+    public function orderConfirmationAction()
+    {
+        return $this->render('P4LouvreBundle:Booking:orderConfirmation.html.twig');
     }
 }
